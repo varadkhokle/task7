429a4ae9dae6d19267abdbda6ea7218cab7807ec
varad <varadkhokle10@gmail.com>
2021-04-10 19:33:15 +0530

    xyz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCSW_fSbSd2F2WRyTrgdfOuoRPtApiTaWs",
+  authDomain: "task7-388de.firebaseapp.com",
+  projectId: "task7-388de",
+  storageBucket: "task7-388de.appspot.com",
+  messagingSenderId: "99881622598",
+  appId: "1:99881622598:web:f6287e2b3404dd3bc57e47"
+  }
 };
